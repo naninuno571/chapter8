2c60229ff9dc44ebc95b2688a5d6f7a878538e26
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-06-09 12:31:00 +0900

    chapter9

--- src/Hero.java
+++ src/Hero.java
@@ -1,6 +1,17 @@
 public class Hero {
     String name;
     int hp;
+    Sword sword;
+    public Hero(String name){//newされた瞬間自動的に一回だけ呼び出される
+        this.hp=100;
+        this.name=name;
+    }
+    public Hero(){//オーバーロードで上のとは別と判断される
+        /*this.hp=100;
+        this.name="ダミー";*/
+        //this.Hero("ダミー");上のを呼べるように見えるができない１
+        this("ダミー");これならできる
+    }
     public void sleep(){
         this.hp=100;
         System.out.println(this.name+"は、眠って回復した!");
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,40 @@
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        Hero h = new Hero();
-        h.name = "ミナト";
-        h.hp = 100;
-        System.out.println("勇者"+h.name+"を生み出しました");
+        Sword s = new Sword();
+        s.name="炎の剣";
+        s.damage=10;
+
+        Hero h1 = new Hero("ミナト");//呼んだ瞬間hp100の勇者ミナトが誕生する
+        System.out.println(h1.hp);
+        //h1.hp = 100;
+        System.out.println("勇者"+h1.name+"を生み出しました");
+
+        /*Hero h2 = new Hero();
+        h2.name = "アサガ";
+        //h2.hp = 100;
+        System.out.println("勇者"+h2.name+"を生み出しました");*/
+
+        Wizard w= new Wizard();
+        w.name="スガワラ";
+        w.hp=50;
+        w.heal(h1);
+        w.heal(h1);
+        w.heal(h1);
 
         Matango m1 = new Matango();
         m1.hp=50;
         m1.suffix='A';
+        h1.sword = s;
+        System.out.println("現在の武器は"+h1.sword.name);
 
         Matango m2 = new Matango();
         m2.hp=48;
         m2.suffix='B';
 
-        h.slip();
+        h1.slip();
         m1.run();
         m2.run();
-        h.run();
+        h1.run();
     }
 }
